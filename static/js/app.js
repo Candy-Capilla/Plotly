f08fee2ea2a64448732bad767cd8b1bfaf4718e4
Candy Capilla <candycapilla@MacBook-Pro-3.local>
2021-01-02 17:18:42 -0500

    push code

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,44 @@
-function createcharts(sample){
-console.log (sample)
+function createPlots () {
+    //read samples.json
+    d3.json("samples.json").then (sampledata => {
+        console.log(sampledata)
+        //get only top 10 otu ids and reverse
+        var otutop10 = sampledata.samples[0].otu_ids.slice(0,10).reverse;
+        console.log(ids)
+        var sampleValues = sampledata.samples[0].samples_values.slice(0,10).reverse();
+        console.log(sampleValues)
+        var labels = sampledata.samples[0].otu_labels.slice(0,10);
+        console.log(labels)
+        //get otu id's for plot
+        var OTU_id = OTU_top.map(id => "OTU" + id);
+        console.log(`OTU IDS: ${OTU_id}`)
+        //create trace
+        var trace = {
+            x: sampleValues,
+            y: OTU_id,
+            text: labels,
+            marker: {
+            color: 'blue'},
+            type: "bar",
+            orientation: "h",
+        };
+        //create data variable
+        var data = [trace];
+
+        //create layout
+        var layout = {
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                l:100,
+                r:100,
+                t:100,
+                b:30
+            }
+        };
 
 
-//.filter on ID
-
-}
-//build dropdown for ID
-d3.json("samples.json").then((samples)=>{
-console.log(samples)
-var selection = d3.select("#selDataset")
-samples.names.forEach((ID)=> {
-    selection.append("option").text(ID).property("value",ID)
-})
-createcharts(sample.names[0])
-})
-function optionChanged (value){
-
-createcharts(value)
+    })
 }
